787ce153fba24fe7c479fac35a1e6857e949a803
Jan Bruzl <bruzl@progenium.cz>
2015-01-07 22:47:20 +0100

    Added thymeleaf support

--- web/src/main/java/cz/muni/fi/cep/web/App.java
+++ web/src/main/java/cz/muni/fi/cep/web/App.java
@@ -3,17 +3,64 @@ package cz.muni.fi.cep.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.ViewResolver;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.TemplateResolver;
 
 @EnableAutoConfiguration
 @Configuration
-@EntityScan(basePackages="cz.muni.fi.cep")
-@ComponentScan(basePackages="cz.muni.fi.cep")
+@EntityScan(basePackages = "cz.muni.fi.cep")
+@ComponentScan(basePackages = "cz.muni.fi.cep")
 public class App {
 
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
 
+	@Bean
+	public TemplateResolver templateResolver() {
+		TemplateResolver tempRes = new SpringResourceTemplateResolver();
+
+		tempRes.setCharacterEncoding("UTF-8");
+		tempRes.setTemplateMode("HTML5");
+		tempRes.setSuffix(".html");
+		tempRes.setPrefix("classpath:/templates/");
+		tempRes.setCacheable(false);
+		return tempRes;
+	}
+
+	@Bean
+	public SpringTemplateEngine templateEngine() {
+		SpringTemplateEngine ste = new SpringTemplateEngine();
+		ste.setTemplateResolver(templateResolver());
+		//ste.setMessageSource(messageSource());
+		return ste;
+	}
+
+	@Bean
+	public ViewResolver viewResolver() {
+		ThymeleafViewResolver tvr = new ThymeleafViewResolver();
+
+		tvr.setCharacterEncoding("UTF-8");
+		tvr.setTemplateEngine(templateEngine());
+
+		return tvr;
+	}
+
+	@Bean
+	@Order(Ordered.HIGHEST_PRECEDENCE)
+	CharacterEncodingFilter characterEncodingFilter() {
+		CharacterEncodingFilter filter = new CharacterEncodingFilter();
+		filter.setEncoding("UTF-8");
+		filter.setForceEncoding(true);
+		return filter;
+	}
 }
--- web/src/main/java/cz/muni/fi/cep/web/controllers/IndexController.java
+++ web/src/main/java/cz/muni/fi/cep/web/controllers/IndexController.java
@@ -1,8 +1,11 @@
 package cz.muni.fi.cep.web.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
-
+	@RequestMapping(value = {"/index"})
+    public void index(){
+    }
 }
