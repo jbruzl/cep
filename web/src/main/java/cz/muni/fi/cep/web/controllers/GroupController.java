b3f2a7144e4dab34661fb14f301d9e163a628fc1
Jan Bruzl <bruzl@progenium.cz>
2015-01-10 11:51:06 +0100

    Introduced group controller + template

--- web/src/main/java/cz/muni/fi/cep/web/controllers/GroupController.java
+++ web/src/main/java/cz/muni/fi/cep/web/controllers/GroupController.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import cz.muni.fi.cep.core.users.api.IdentityService;
+import cz.muni.fi.cep.core.users.entities.CepGroupEntity;
 
 @Controller
 @RequestMapping(value = { "/skupiny" })
@@ -17,5 +20,17 @@ public class GroupController {
 	public String index(Model model) {
 		model.addAttribute("groups", identityService.getAllGroups());
 		return "/groups/groups";
+	}
+	
+	@RequestMapping(value= {"/pridat"})
+	public String addGroup(Model model) {
+		model.addAttribute("group", new CepGroupEntity());
+		return "/groups/add";
+	}
+	
+	@RequestMapping(value= {"/pridat-submit"}, method=RequestMethod.POST)
+	public String createGroup(Model model, final CepGroupEntity group) {
+		identityService.createGroup(group);
+		return "redirect:/skupiny";
 	}
 }
