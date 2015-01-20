d8398154fc6f216b18cafe2584750c57fae0568b
Jan Bruzl <bruzl@progenium.cz>
2015-01-20 22:34:37 +0100

    ContactType updated

--- api/src/main/java/cz/muni/fi/cep/api/DTO/ContactType.java
+++ api/src/main/java/cz/muni/fi/cep/api/DTO/ContactType.java
@@ -1,5 +1,31 @@
 package cz.muni.fi.cep.api.DTO;
 
 public enum ContactType {
-	SMS, PHONE, EMAIL;
+	SMS("SMS"), PHONE("PHONE"), EMAIL("EMAIL");
+
+	private final String value;
+
+	ContactType(String value) {
+		this.value = value;
+	}
+
+	public static ContactType fromValue(String value) {
+		if (value != null) {
+			for (ContactType contactType : values()) {
+				if (contactType.value.equals(value)) {
+					return contactType;
+				}
+			}
+		}
+		return getDefault();
+	}
+
+	public String toValue() {
+		return value;
+	}
+
+	private static ContactType getDefault() {
+		return EMAIL;
+	}
+
 }
