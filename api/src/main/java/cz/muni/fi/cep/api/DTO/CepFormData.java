bb7241a6e816d9b3eab5c03070670fd45992eea2
Jan Bruzl <bruzl@progenium.cz>
2015-02-10 22:15:00 +0100

    Cep representation of FormProperty and FormData

---
+++ api/src/main/java/cz/muni/fi/cep/api/DTO/CepFormData.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package cz.muni.fi.cep.api.DTO;
+
+import java.util.List;
+
+import org.activiti.engine.form.FormData;
+import org.activiti.engine.form.FormProperty;
+
+/**
+ * Cep implementation of {@link FormData}.
+ * 
+ * @author Jan Bruzl
+ *
+ */
+public class CepFormData implements FormData {
+	private String formKey;
+	private String deploymentId;
+	List<FormProperty> formProperties;
+	
+	public void setFormKey(String formKey) {
+		this.formKey = formKey;
+	}
+
+	public void setDeploymentId(String deploymentId) {
+		this.deploymentId = deploymentId;
+	}
+
+	public void setFormProperties(List<FormProperty> formProperties) {
+		this.formProperties = formProperties;
+	}
+
+	@Override
+	public String getFormKey() {
+		return formKey;
+	}
+
+	@Override
+	public String getDeploymentId() {
+		return deploymentId;
+	}
+
+	@Override
+	public List<FormProperty> getFormProperties() {
+		return formProperties;
+	}
+
+}
---
+++ api/src/main/java/cz/muni/fi/cep/api/DTO/CepFormProperty.java
@@ -0,0 +1,105 @@
+/**
+ * 
+ */
+package cz.muni.fi.cep.api.DTO;
+
+import org.activiti.engine.form.FormProperty;
+import org.activiti.engine.form.FormType;
+
+/**
+ * Cep implementation of {@link FormProperty}.
+ * 
+ * Added {@link #input} variable for form input representation. So class could
+ * be used not only as form description but also as container for submitted
+ * form.
+ * 
+ * @author Jan Bruzl
+ *
+ */
+public class CepFormProperty implements FormProperty {
+	private static final long serialVersionUID = -1362637926496741494L;
+	private String id;
+	private String name;
+	private String value;
+	private boolean readable;
+	private boolean writable;
+	private boolean required;
+	private FormType type;
+	private Object input;
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public void setReadable(boolean readable) {
+		this.readable = readable;
+	}
+
+	public void setWritable(boolean writable) {
+		this.writable = writable;
+	}
+
+	public void setRequired(boolean required) {
+		this.required = required;
+	}
+
+	public void setType(FormType type) {
+		this.type = type;
+	}
+
+	public void setInput(Object input) {
+		this.input = input;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	public Object getInput() {
+		return input;
+	}
+
+	@Override
+	public String getId() {
+		return id;
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public FormType getType() {
+		return type;
+	}
+
+	@Override
+	public String getValue() {
+		return value;
+	}
+
+	@Override
+	public boolean isReadable() {
+		return readable;
+	}
+
+	@Override
+	public boolean isWritable() {
+		return writable;
+	}
+
+	@Override
+	public boolean isRequired() {
+		return required;
+	}
+
+}
